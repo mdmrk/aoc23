71fca2d465db6b5ac851f66611676d02cfc6b192
mdmrk <mariodavo20@gmail.com>
2025-05-09 15:46:43 +0200

    14

--- src/days/day014/mod.rs
+++ src/days/day014/mod.rs
@@ -1,5 +1,11 @@
+use std::io::{self, stdin};
+
 use crate::days::Day;
 pub struct Day014;
+static ROUND_ROCK: char = 'O';
+static CUBE_ROCK: char = '#';
+static EMPTY: char = '.';
+
 impl Day for Day014 {
     fn problem(&self) -> &'static str {
         include_str!("problem.txt")
@@ -9,6 +15,52 @@ impl Day for Day014 {
     }
     fn part1(&self, input: &String) -> Result<String, String> {
         let mut result: usize = 0;
+
+        let mut map: Vec<Vec<_>> = input
+            .lines()
+            .map(|l| l.chars().collect::<Vec<char>>())
+            .collect();
+        for i in &map {
+            println!("{:?}", i);
+        }
+        println!();
+
+        let cols = map[0].len();
+
+        for c in 0..cols {
+            'o: loop {
+                let mut i: usize = 0;
+
+                'i: while i < map.len() {
+                    if map[i][c] == ROUND_ROCK {
+                        if i > 0 {
+                            let j = i - 1;
+
+                            if map[j][c] == EMPTY {
+                                map[j][c] = ROUND_ROCK;
+                                map[i][c] = EMPTY;
+                                break 'i;
+                            }
+                        }
+                    }
+                    // let mut buf = String::new();
+                    // println!("i: {} j: {}", i, i);
+                    // stdin().read_line(&mut buf);
+                    i += 1;
+                }
+                if i == map.len() {
+                    break 'o;
+                }
+            }
+        }
+        for (i, line) in map.iter().enumerate() {
+            for c in line {
+                if *c == ROUND_ROCK {
+                    result += map.len() - i;
+                }
+            }
+        }
+
         Ok(result.to_string())
     }
     fn part2(&self, input: &String) -> Result<String, String> {
